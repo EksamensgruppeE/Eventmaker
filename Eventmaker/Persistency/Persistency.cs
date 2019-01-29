48a8294eff5cdfc3dc2e231e97713e2cfbf9782f
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2019-01-29 10:51:53 +0100

    Add project files.

---
+++ Eventmaker/Persistency/Persistency.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Eventmaker.Model;
+using Newtonsoft.Json;
+
+namespace NoteMVVM
+{
+    //Json.Net er downloaded til projektet via NuGet: Højreklik på projektet -> Manage NuGet Package
+
+    class PersistencyService
+    {
+        private static string JsonFileName = "Events.json";
+
+        public static async void SaveEventsAsJsonAsync(ObservableCollection<Event> notes)
+        {
+            string notesJsonString = JsonConvert.SerializeObject(notes);
+            SerializeNotesFileAsync(notesJsonString, JsonFileName);
+        }
+
+        public static async Task<List<Event>> LoadEventsFromJsonAsync()
+        {
+            string notesJsonString = await DeserializeNotesFileAsync(JsonFileName);
+            if (notesJsonString != null)
+                return (List<Event>) JsonConvert.DeserializeObject(notesJsonString, typeof (List<Event>));
+            return null;
+        }
+
+       
+
+        private static async void SerializeNotesFileAsync(string notesJsonString, string fileName)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(localFile, notesJsonString);
+        }
+
+        
+        private static async Task<string> DeserializeNotesFileAsync(string fileName)
+        {
+            try
+            {
+                StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+                return await FileIO.ReadTextAsync(localFile);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageDialogHelper.Show("Loading for the first time? - Try Add and Save some Notes before trying to Save for the first time", "File not Found");
+                return null;
+            }
+        }
+
+
+        private class MessageDialogHelper
+        {
+            public static async void Show(string content, string title)
+            {
+                MessageDialog messageDialog = new MessageDialog(content, title);
+                await messageDialog.ShowAsync();
+            }
+        }
+
+    }
+}
