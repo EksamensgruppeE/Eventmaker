d0a412cfb2ce5507aeba68a724b5da12965fdf56
Kristian Hornbøll <kris195y@edu.easj.dk>
2019-02-01 09:03:30 +0100

    updates

--- Eventmaker/Model/Event.cs
+++ Eventmaker/Model/Event.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Eventmaker.Model
 {
-    class Event
+    class Event 
     {
+        // Properties
+        public DateTime DateTime { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Place { get; set; }
+
+        // Constructor
+        public Event(DateTime dateTime, int id, string description, string name, string place)
+        {
+            DateTime = dateTime;
+            Id = id;
+            Description = description;
+            Name = name;
+            Place = place;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(DateTime)}: {DateTime}, {nameof(Id)}: {Id}, {nameof(Description)}: {Description}, {nameof(Name)}: {Name}, {nameof(Place)}: {Place}";
+        }
     }
 }
--- Eventmaker/ViewModel/EventViewModel.cs
+++ Eventmaker/ViewModel/EventViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eventmaker.Model;
 
 namespace Eventmaker.ViewModel
 {
     class EventViewModel
     {
+        public EventCatalogSingleton EventCatalogSingleton { get; set; }
+
+        public EventViewModel()
+        {
+            EventCatalogSingleton = EventCatalogSingleton.Instance;
+        }
     }
+
+    
 }
